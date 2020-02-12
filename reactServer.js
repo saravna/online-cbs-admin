b8796c455cbe92dc049d33cc70b89906e604e5d9
saravana <saravanakumar.ravichandran@codingmart.com>
2020-02-13 00:24:42 +0530

    URL change

--- reactServer.js
+++ reactServer.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-var http_port = 3001;
+var http_port = 3000;
 if (process.env.NODE_ENV === 'production')
-  http_port = 3001;
+  http_port = 3000;
 else if (process.env.NODE_ENV === 'test')
-  http_port = 3001;
+  http_port = 3000;
 const path = require('path');
 class Server {
   constructor() {
